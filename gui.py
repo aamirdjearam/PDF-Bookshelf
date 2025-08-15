7981c21bed26a10f043d2a3a454af25b10dc17fb
Aamir Djearam <djearam2@gmail.com>
2025-08-15 10:27:24 -0400

    Virtual Environment setup

---
+++ gui.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+main_window = tk.Tk()
+main_window.title("My Application")
+main_window.geometry("800x600")
+
+
+main_window.mainloop()
---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+folder_path = r"E:\PDF Reader"
+pdf_files = []
+for files in os.listdir(folder_path):
+    if files.lower().endswith(".txt)"):
+        pdf_files.append(pdfs)
+print(files)
